d39791a6293a962d3e03b496c35be8fb39eb7b8b
Egor <alexandrovich2572@gmail.com>
2019-10-01 21:35:59 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.numbr = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.numbr = Math.round((this.min+this.max)/2);
+        return this.numbr
     }
-
+  
     lower() {
-
+      this.max = this.numbr;
+      return this.max 
     }
-
+  
     greater() {
-
+      this.min = this.numbr;
+      return this.min
     }
-}
+  }
 
 module.exports = GuessingGame;
